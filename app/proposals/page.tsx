952840e549b1a1d63dd437d7ce34588385211c16
Pietro Pazini Passos <pietroppassos@gmail.com>
2025-02-06 17:15:53 -0300

    feat: create get contact from Homio

--- app/proposals/page.tsx
+++ app/proposals/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Form from '@/components/Form'
 import { getOpportunities } from '@/lib/requests';
+import { getContacts } from '@/lib/requests';
 
 const proposals = async () => {
   const opportunity = await getOpportunities('AUqyVuQlzu7fAFupcoNl');
   console.log(opportunity);
+  const contact = await getContacts('XB6C3FsgPmyRctL5Elap');
+  console.log(contact);
   return (
     <div className='mt-24 ml-28 font-bold'>
       <h1 className='text-gray-900 text-3xl'>Criar nova proposta</h1>
--- lib/requests.ts
+++ lib/requests.ts
@@ -18,3 +18,28 @@ export async function getOpportunities(opportunityId: string): Promise<object> {
         throw new Error("Falha ao obter as oportunidades.");
     }
 }
+
+export async function getContacts(ContactId: string): Promise<object> {
+    try {
+      const USERNAME = process.env.HOMIO_API_MIVITA_USER;
+      const PASSWORD = process.env.HOMIO_API_MIVITA_PASS;
+      const body = {
+        locationId: "Ew4LzbKZmyOVYwv4iwDI",
+        pageLimit: 1, 
+        page: 1
+      };
+      const response = await mivita.get<object>(
+        "/contacts/?id=" + ContactId,
+        {
+          headers: {
+            Authorization: "Basic " + Buffer.from(`${USERNAME}:${PASSWORD}`).toString("base64")
+          },
+          data: body
+        }
+      );
+      return response.data;
+    } catch (error) {
+        console.error("Erro ao obter os contatos", error);
+        throw new Error("Falha ao obter os contatos.");
+    }
+}
