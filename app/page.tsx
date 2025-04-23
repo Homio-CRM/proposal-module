bb07cc162c84db33a0bf6ce1c62eeee5f0db454e
Pietro Pazini Passos <pietroppassos@gmail.com>
2025-04-23 10:28:37 -0300

    feat: fixing errors to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Form from '@/components/Form'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <div className='mt-6 mx-14 font-bold'>
-      <Form />
+      <Suspense fallback="Carregando">
+        <Form />
+      </Suspense>
     </div > 
   )
 }
--- app/proposals/page.tsx
+++ app/proposals/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Form from '@/components/Form'
 
 const proposals = async () => {
 
   return (
     <div className='mt-24 ml-28 font-bold'>
-      <Form />
+      <Suspense fallback="Carregando">
+        <Form />
+      </Suspense>
     </div >
 
   )
